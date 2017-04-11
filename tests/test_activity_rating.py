33c032bc430c7dab1f35ef5c3bde06fa57c05d18
Raimon Esteve <raimon@nan-tic.com>
2017-04-11 13:51:27 +0200

    FIX tests

--- tests/test_activity_rating.py
+++ tests/test_activity_rating.py
@@ -1,27 +1,17 @@
-#!/usr/bin/env python
+# This file is part of the product_sequence module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(unittest.TestCase):
-    'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('activity_rating')
-
-    def test0005views(self):
-        'Test views'
-        test_view('activity_rating')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
-
+class ActivityRatingTestCase(ModuleTestCase):
+    'Test Activity Rating module'
+    module = 'activity_rating'
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        ActivityRatingTestCase))
     return suite
